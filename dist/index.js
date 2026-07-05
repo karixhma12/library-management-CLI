b8121408524d2f32b09cbe282577d83c53a61916
Karishma <karishma.7022@gmail.com>
2026-07-05 16:06:35 +0530

    Add Partial for book updates

--- dist/index.js
+++ dist/index.js
@@ -13,4 +13,10 @@ let book1 = {
 };
 displayBookSummary(book1);
 displayBookSummary({ title: "Dune", author: "Frank Herbert" });
+function updateBook(updatedBook) {
+    console.log("Updating book with : ", updatedBook);
+}
+updateBook({ year: 1938 });
+updateBook({ genre: "Adventure", status: "borrowed" });
+updateBook({});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ interface Book{
 }
 
 type BookSummary = Pick<Book, 'title' | 'author'>;
+type UpdatableFields = Pick<Book, 'title' | 'author' | 'year' | 'genre' | 'status'>;
+type UpdateBook = Partial<UpdatableFields>;
 
 const displayBookSummary = function(book : BookSummary):void{
     console.log(`Title : ${book.title}, Author : ${book.author}`);
@@ -24,3 +26,14 @@ let book1 : Book = {
 
 displayBookSummary(book1);
 displayBookSummary({title:"Dune",author:"Frank Herbert"});
+
+
+function updateBook(updatedBook : UpdateBook):void{
+    console.log("Updating book with : ", updatedBook);
+}
+
+updateBook({ year: 1938 });
+updateBook({ genre: "Adventure", status: "borrowed" });
+updateBook({});
+
+
